2b9f7f7b2d7d54372ac9a70a6dd3990cd9832c4a
Dylan Anthony <43723790+dbanty@users.noreply.github.com>
2023-03-01 22:05:14 +0000

    feat: Allow passing GitHub tokens. Closes #2

--- get_knope_version.py
+++ get_knope_version.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import json
+from os import environ
 import ssl
 import sys
-from urllib import request
+from urllib.request import Request, urlopen
 
 input_version = sys.argv[1]
+github_token = environ.get("GITHUB_TOKEN")
 
 if input_version != "LATEST":
     if not input_version.startswith("v"):
@@ -14,6 +16,10 @@ if input_version != "LATEST":
     sys.exit(0)
 
 context = ssl.create_default_context()
-response = request.urlopen("https://api.github.com/repos/knope-dev/knope/releases?per_page=1", context=context)
+request = Request("https://api.github.com/repos/knope-dev/knope/releases?per_page=1")
+if github_token:
+    auth_value = f"Bearer {github_token}"
+    request.add_header("authorization", auth_value)
+response = urlopen(request, context=context)
 data = json.loads(response.read())
 print(data[0]["tag_name"])

407e9ef7c272d2dd53a4e71e39a7839e29933c48
Dylan Anthony <dbanty@users.noreply.github.com>
2024-04-14 13:53:10 -0600

    feat: Print out the auto-downloading version

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,23 +4,25 @@ set -euo pipefail
 
 VERSION=$1
 
-# Empty string means get latest, leave it empty
-if [[ -z "${VERSION}" ]]; then
-  TAG=""
-# The tag is the version prefixed with a 'v' (if not already provided)
-elif [[ ! ${VERSION} =~ ^v.*  ]]; then
-  TAG="v${VERSION}"
-else
-  TAG="${VERSION}"
-fi
+# If a version was set, do some massaging
+if [[ -n "${VERSION}" ]]; then
+  # The tag is the version prefixed with a 'v' (if not already provided)
+  if [[ ! ${VERSION} =~ ^v.*  ]]; then
+    TAG="v${VERSION}"
+  else
+    TAG="${VERSION}"
+  fi
 
-# If the version is 0.16.2 or greater, the tag is prefixed with knope/
-if [[ ${TAG} == "v0.16.2" || ${TAG} > "v0.16.2" ]]; then
-  TAG="knope/${TAG}"
+  # If the version is 0.16.2 or greater, the tag is prefixed with knope/
+  if [[ ${TAG} == "v0.16.2" || ${TAG} > "v0.16.2" ]]; then
+    TAG="knope/${TAG}"
+  fi
+else # If the tag is empty, get the latest release
+  TAG=$(gh release list --json tagName --jq '.[].tagName' --repo "knope-dev/knope"  | grep knope | head -n 1)
 fi
 
 # Use the GitHub CLI to download
-
+echo "Downloading ${TAG}"
 gh release download "${TAG}" --repo "knope-dev/knope" --pattern "*linux*"
 TAR_FILE=$(ls knope-*.tgz)
 tar -xvf "$TAR_FILE"
